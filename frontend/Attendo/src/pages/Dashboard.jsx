4149417859cac7ffc49071d78aa60ae0e805f401
swawiye <sovereign.wawiye@gmail.com>
2025-07-10 09:08:21 +0300

    fetching course data and attendance

--- frontend/Attendo/src/pages/Dashboard.jsx
+++ frontend/Attendo/src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 
 export default function Dash() {
@@ -6,6 +6,19 @@ export default function Dash() {
     const [userCourse, setUserCourse] = useState("");
     const [attendanceHist, setAttendanceHist] = useState([]);
     const navigate = useNavigate();
+
+    useEffect(() => {
+        // Fetch user units and attendance history
+        const fetchData = async () => {
+            const res = await fetch("/api/dashboard") //from the backend
+            const data = await res.json() //parse the data to json format
+
+            setUnits(data.units); //fetch the units
+            setAttendanceHist(data.attendanceHist); //fetch the attendance history
+            setUserCourse(data.course); //fetch the course
+        };
+        fetchData();
+    }, []);
 
     return (
     <>
