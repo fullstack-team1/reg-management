7f70e4c089c95ca23b3fb4ddbd49d40bd2d08422
swawiye <sovereign.wawiye@gmail.com>
2025-07-10 09:29:59 +0300

    logic mapping

--- frontend/Attendo/src/pages/Dashboard.jsx
+++ frontend/Attendo/src/pages/Dashboard.jsx
@@ -41,10 +41,43 @@ export default function Dash() {
     return (
     <>
         <div>
-            <h1>Welcome Student</h1>
+            <h1>Welcome, {userCourse}</h1>
+
+            <button onClick={handleLogout}>Logout</button>
+
+            <h2>Mark Attendance</h2>
+
+            <ul>
+                {units.map((unit) => (
+                    <li key={unit.id}>
+
+                        <span>{unit.name}</span>
+                        <span>{unit.date}</span>
+                        <span>{unit.start_time}</span>
+                        <span>{unit.end_time}</span>
+
+                        <button
+                            onClick={() => handleAttendance(unit.id)}
+                            diasbled={ new Date() <  new Date(unit.date)}
+                        >
+                            Attend
+                        </button>
+                    </li>
+                ))}
+            </ul>
+
+            <h2>Attendance History</h2>
+
+            <ul>
+                {attendanceHist.map((attendance) => (
+                    <li key={attendance.id}>
+                        {attendance.unit_name} - {attendance.marked_time}
+                    </li>
+                ))}
+            </ul>
         </div>
     </>
-)
+    )
 };
 
     
